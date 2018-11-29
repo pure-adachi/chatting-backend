40166dc5846873dc9d46d33ffd25de4b6f377d03
ta3923 <haimaki4222@gmail.com>
2018-11-30 00:10:38 +0900

    Add create talk room mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :login, mutation: UserMutations::Login
     field :create_message, mutation: MessageMutations::Create
+    field :create_talk_room, mutation: TalkRoomMutations::Create
   end
 end
